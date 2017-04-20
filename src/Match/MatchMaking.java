a71e929ce4a0a95e2630979d0a08cad0306e0858
Kazachenko1998 <Kazachenko1998@users.github.com>
2017-04-20 21:35:34 +0300

    И. атака

--- src/Match/MatchMaking.java
+++ src/Match/MatchMaking.java
@@ -1,21 +1,49 @@
 package Match;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 import static InitializationGame.WindowMatchMaking1.launch;
 
 public class MatchMaking {
 
     public static void damageVisual(Pane pane) {
-       ImageView imageView = (ImageView) pane.getChildren().get(0);
-       imageView.setLayoutX(imageView.getX()+1);
+        ImageView imageView = (ImageView) pane.getChildren().get(2);
 
-System.out.print(pane);
+        AnimationTimer gameLoop = new AnimationTimer() {
+            int k = 1;
+
+            @Override
+            public void handle(long now) {
+
+                if (imageView.getLayoutX() < 640) imageView.setLayoutX(imageView.getLayoutX() + 30 * k);
+                else {
+                    pane.getChildren().get(pane.getChildren().size() - 1).setLayoutY(200);
+                    k *= -1;
+                      imageView.setLayoutX(imageView.getLayoutX() - 40);
+                }
+                if (imageView.getLayoutX() < 50) {
+                    imageView.setLayoutX(50);
+                    pane.getChildren().get(pane.getChildren().size() - 1).setLayoutY(-1000);
+                    stop();
+                }
+            }
+        };
+
+        gameLoop.start();
+
+
     }
 
     public static void treatmentVisual(Pane pane) {
+
+        pane.getChildren().addAll();
     }
 
 
@@ -32,6 +60,13 @@ System.out.print(pane);
 
     public static void battleProcess(Pane pane, Player player1, Player player2) {
         launch(pane, player1, player2);
+        ImageView imageView1 = new ImageView(new Image(new File("src\\Picture\\Heroes\\GeneralSkills\\dpsHero.png").toURI().toString()));
+        imageView1.setLayoutX(950);
+        imageView1.setLayoutY(-1000);
+        imageView1.setFitWidth(300);
+        imageView1.setFitHeight(300);
+
+        pane.getChildren().addAll(imageView1);
 
         boolean inBattle = true;
         while (inBattle) {
